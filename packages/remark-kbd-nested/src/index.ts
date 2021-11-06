75621db65c730de732b4fee88a63d3e95800e167
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-11-06 23:01:30 +0530

    docs(remark-kbd-nested): add doc comments

--- packages/remark-kbd-nested/src/index.ts
+++ packages/remark-kbd-nested/src/index.ts
@@ -1,8 +1,46 @@
+/**
+ * [remark](https://github.com/remarkjs/remark) plugin to support
+ * keyboard and variable sequences. See {@link
+ * micromark-extension-kbd-nested} for syntax and options.
+ *
+ * ## Use
+ *
+ * ```js
+ * import { unified } from "unified";
+ * import remarkParse from "remark-parse";
+ * import { remarkKbdNested } from "remark-kbd-nested";
+ * import remarkRehype from "remark-rehype";
+ * import rehypeStringify from "rehype-stringify";
+ *
+ * main();
+ *
+ * async function main() {
+ *   const file = await unified()
+ *     .use(remarkParse)
+ *     .use(remarkKbdNested)
+ *     .use(remarkRehype)
+ *     .use(rehypeStringify)
+ *     .process("Press ||| ||Ctrl|| + ||//key//|| ||| to trigger the action.");
+ *
+ *   console.log(String(file));
+ * }
+ * ```
+ *
+ * Yields:
+ *
+ * ```html
+ * <p>Press <kbd><kbd>Ctrl</kbd></kbd> + <kbd><var>key</var></kbd></kbd> to trigger the action.</p>
+ * ```
+ *
+ * @module
+ */
+
 import { IOptions, syntax as kbd } from "micromark-extension-kbd-nested";
 import type { Plugin } from "unified";
 import type { Root } from "mdast";
 import type { Extension as FromMarkdownExtension } from "mdast-util-from-markdown";
 
+/** {@link mdast-util-from-markdown} extension to convert keyboard and variable sequences into [rehype](https://github.com/rehypejs/rehype/)-compatible `kbd` and `var` elements, respectively. */
 const fromMarkdown: FromMarkdownExtension = Object.freeze({
   canContainEols: ["keyboard"],
   enter: {
@@ -41,6 +79,14 @@ const fromMarkdown: FromMarkdownExtension = Object.freeze({
   },
 });
 
+/**
+ * remark plugin to convert keyboard and variable sequences into
+ * [rehype](https://github.com/rehypejs/rehype/)-compatible `<kbd>`
+ * and `<var>` elements respectively. See {@link
+ * micromark-extension-kbd-nested} for syntax and options.
+ *
+ * @param options
+ */
 export const remarkKbdNested: Plugin<IOptions[], Root> = function (
   options: IOptions = {},
 ): void {

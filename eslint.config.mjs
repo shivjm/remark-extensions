759a9129bd24978accaa9e418b828bf0f9c7072a
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2025-09-16 21:38:37 +0530

    chore: fix linting (redux)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,32 @@
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
+
+const RULES = {
+  ts: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+  },
+};
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
   {
     languageOptions: {
       parserOptions: {
-        project: ["../../tsconfig.eslint.json", "./tsconfig.json"],
-        tsconfigRootDir: import.meta.dirname,
+        parser: tseslint.parser,
+        project: ["../../tsconfig.eslint.json", "tsconfig.json"],
       },
     },
+    rules: RULES.ts,
   },
 );

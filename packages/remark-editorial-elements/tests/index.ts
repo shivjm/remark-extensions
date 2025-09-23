2a14f99bff213e027731596fcac482f46144bd6a
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2025-09-23 21:56:37 +0530

    test(remark-editorial-elements): add basic tests using builtin test runner

--- packages/remark-editorial-elements/tests/index.ts
+++ packages/remark-editorial-elements/tests/index.ts
@@ -1,135 +1,62 @@
-import "mocha";
-import { assert } from "chai";
+import { describe, it } from "node:test";
+import { strict as assert } from "node:assert";
 
-import { html, syntax } from "../src/index.js";
+import type { Root, RootContent, Parent, ElementContent, Element } from "hast";
+import type {} from "micromark-extension-directive";
 
-import { micromark } from "micromark";
+import { editingPlugin, CreateDateTimeFn, Options } from "../src/index.js";
 
-type Cases =
-  | readonly [string, string | undefined, string][]
-  | readonly [string, string | undefined, string, string | undefined][];
+type Case = [Root, CreateDateTimeFn | undefined, Root];
 
-function runCases(cases: Cases) {
-  for (const [input, delimiter, expected, variableDelimiter] of cases) {
-    it(`delimiter: ${JSON.stringify(delimiter)}, input: ${JSON.stringify(
-      input,
-    )}`, () => {
-      assert.equal(
-        micromark(input, {
-          extensions: [syntax({ delimiter, variableDelimiter })],
-          htmlExtensions: [html],
-        }),
-        expected,
-      );
+const CASES: readonly Case[] = [
+  [{ type: "root", children: [] }, undefined, { type: "root", children: [] }],
+  [
+    {
+      type: "root",
+      children: [
+        {
+          type: "textDirective",
+          name: "ins",
+          attributes: {},
+          children: [],
+        } as RootContent,
+      ],
+    },
+    undefined,
+    {
+      type: "root",
+      children: [
+        {
+          attributes: {
+            class: "inline",
+          },
+          children: [],
+          data: {
+            hName: "ins",
+            hProperties: {
+              class: "inline",
+            },
+          },
+          name: "ins",
+          type: "textDirective",
+        } as RootContent,
+      ],
+    },
+  ],
+];
+
+describe("editing should work", () => {
+  let i = 0;
+  for (const [input, createDateTime, expected] of CASES) {
+    i++;
+    it(`editing should work ${i}`, () => {
+      const options: Partial<Options> = {};
+      if (createDateTime !== undefined) {
+        options.createDateTime = createDateTime;
+      }
+      const transformer = editingPlugin(options);
+      transformer(input, false);
+      assert.deepEqual(input, expected);
     });
   }
-}
-
-describe("works for simple cases", () => {
-  runCases([
-    ["||Ctrl||", undefined, "<p><kbd>Ctrl</kbd></p>"],
-    ["| ||Ctrl||", undefined, "<p>| <kbd>Ctrl</kbd></p>"],
-    ["| ||Ctrl|| |", undefined, "<p>| <kbd>Ctrl</kbd> |</p>"],
-    ["||Ctrl||", "|", "<p><kbd>Ctrl</kbd></p>"],
-    ["|| Ctrl ||", "|", "<p><kbd>Ctrl</kbd></p>"],
-    ["|| npm run build ||", undefined, "<p><kbd>npm run build</kbd></p>"],
-    ["||a|| bc ||d", undefined, "<p><kbd>a</kbd> bc ||d</p>"],
-  ]);
-});
-
-describe("handles different delimiters", () => {
-  runCases([
-    ["++Ctrl++", "|", "<p>++Ctrl++</p>"],
-    ["++Ctrl++", "+", "<p><kbd>Ctrl</kbd></p>"],
-  ]);
-});
-
-describe("handles escaping", () => {
-  runCases([
-    ["||\\|||", undefined, "<p><kbd>|</kbd></p>"],
-    ["|| \\| ||", undefined, "<p><kbd>|</kbd></p>"],
-    ["|| \\| \\| || \\|", undefined, "<p><kbd>| |</kbd> |</p>"],
-    ["|| \\  ||", undefined, "<p><kbd> </kbd></p>"],
-    ["|| \\\\  ||", undefined, "<p><kbd>\\</kbd></p>"],
-    ["++ \\  ++", "+", "<p><kbd> </kbd></p>"],
-  ]);
-});
-
-describe("handles nesting", () => {
-  runCases([
-    [
-      "|||  ||Ctrl||  +  ||Alt|| + ||x||  |||",
-      undefined,
-      "<p><kbd><kbd>Ctrl</kbd>  +  <kbd>Alt</kbd> + <kbd>x</kbd></kbd></p>",
-    ],
-    [
-      "||||| ||Ctrl|| + |||| Alt |||| + |||x||| |||||",
-      undefined,
-      "<p><kbd><kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>x</kbd></kbd></p>",
-    ],
-  ]);
-});
-
-describe("handles variable sections", () => {
-  runCases([
-    ["||//k//||", undefined, "<p><kbd><var>k</var></kbd></p>", undefined],
-    [
-      "||| ||Ctrl|| + ||// key //|| |||",
-      undefined,
-      "<p><kbd><kbd>Ctrl</kbd> + <kbd><var>key</var></kbd></kbd></p>",
-      undefined,
-    ],
-    ["++||k||   ++", "+", "<p><kbd><var>k</var></kbd></p>", "|"],
-    [
-      "!!! !!Ctrl!! + !!   __key   __!! !!!",
-      "!",
-      "<p><kbd><kbd>Ctrl</kbd> + <kbd><var>key</var></kbd></kbd></p>",
-      "_",
-    ],
-  ]);
-});
-
-describe("handles everything together", () => {
-  runCases([
-    [
-      "@@@@@ @@Ctrl@@ + @@@@ Alt @@@@ + @@@ \\@@@@ @@@@@",
-      "@",
-      "<p><kbd><kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>@</kbd></kbd></p>",
-    ],
-    [
-      "@@@@@ @@Ctrl@@ + @@@@ Alt @@@@ + @@@ \\ @@@ @@@@@",
-      "@",
-      "<p><kbd><kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd> </kbd></kbd></p>",
-    ],
-    [
-      "Press ^^^ ^^Ctrl^^ + ^^ \\^ ^^ ^^^, then ^^ //key// ^^.",
-      "^",
-      "<p>Press <kbd><kbd>Ctrl</kbd> + <kbd>^</kbd></kbd>, then <kbd><var>key</var></kbd>.</p>",
-    ],
-  ]);
-});
-
-describe("doesn’t break anything else", () => {
-  runCases([
-    [
-      "Hello `||there||` my friends",
-      undefined,
-      "<p>Hello <code>||there||</code> my friends</p>",
-    ],
-    [
-      "Hello `there` ||q|| my friends",
-      undefined,
-      "<p>Hello <code>there</code> <kbd>q</kbd> my friends</p>",
-    ],
-    [
-      "Hello `there` |q my friends",
-      undefined,
-      "<p>Hello <code>there</code> |q my friends</p>",
-    ],
-    [
-      "Hello `there` |q| my friends",
-      undefined,
-      "<p>Hello <code>there</code> |q| my friends</p>",
-    ],
-  ]);
 });

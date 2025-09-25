bf8b24ee0de4d6c432beffaece6a8b46375859f5
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2025-09-26 02:29:55 +0530

    feat(remark-textras): add `:work`

--- packages/remark-textras/src/index.ts
+++ packages/remark-textras/src/index.ts
@@ -1,4 +1,5 @@
 import type { Root, Parent } from "hast";
+import { h } from "hastscript";
 import { visit } from "unist-util-visit";
 import { getFirstAndLastText, addClass } from "./ast.js";
 // This is required to augment the AST type, but unfortunately can’t
@@ -7,6 +8,23 @@ import { getFirstAndLastText, addClass } from "./ast.js";
 
 const OPENING_QUOTE = /^(?:‘|“|’|”)/;
 const CLOSING_QUOTE = /(?:‘|“|’|”)$/;
+
+const YEAR_SPACE = Object.freeze({ type: "text", value: " " });
+
+const QUOTE_SPACE = Object.freeze({
+  type: "text",
+  value: "\u202F",
+});
+
+const OPENING_QUOTE_AST = Object.freeze({
+  type: "text",
+  value: "‘",
+});
+
+const CLOSING_QUOTE_AST = Object.freeze({
+  type: "text",
+  value: "’",
+});
 
 export function textrasPlugin() {
   return transformer;
@@ -44,9 +62,56 @@ export function textrasPlugin() {
     }
   }
 
+  function transformWorkTitle(node: Parent) {
+    node.data = {
+      ...node.data,
+      hName: "cite",
+      hProperties: { class: "work-title" },
+    };
+
+    const { year, quoted } = (node as any).attributes || {};
+
+    if (quoted !== undefined) {
+      const [first, last] = getFirstAndLastText(node);
+
+      if (OPENING_QUOTE.test(first ?? "")) {
+        node.children.unshift(QUOTE_SPACE);
+      }
+
+      node.children.unshift(OPENING_QUOTE_AST);
+
+      if (CLOSING_QUOTE.test(last ?? "")) {
+        node.children.push(QUOTE_SPACE);
+      }
+
+      node.children.push(CLOSING_QUOTE_AST);
+    }
+
+    if (year !== undefined) {
+      const time = h("time");
+      time.data = {
+        hName: "time",
+        hProperties: {
+          class: "work-vintage",
+          datetime: year,
+        },
+      };
+
+      time.children = [
+        {
+          type: "text",
+          value: `(${year})`,
+        },
+      ];
+      node.children.push(YEAR_SPACE);
+      node.children.push(time);
+    }
+  }
+
   function transformer(tree: Root, _file: unknown) {
     visit(tree, { type: "textDirective", name: "ordinal" }, ordinalVisitor);
     visit(tree, { type: "textDirective", name: "quote" }, quoteVisitor);
+    visit(tree, { type: "textDirective", name: "work" }, transformWorkTitle);
 
     for (const [name, element] of Object.entries({
       var: "var",
